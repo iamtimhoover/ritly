389bacd89f10d0a8a38e718183aeb97630124015
Tim Hoover <timothyhoov@gmail.com>
2013-08-25 16:15:54 -0400

    Create and Save link with random hash_code

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,9 +1,21 @@
 class UrlsController < ApplicationController
-  def create
+  
+	def index
+		@urls = Url.all
+	end
+  def new
+  	@url = Url.new
   end
 
-  def new
-  end
+  def create
+  	safe_url = params.require(:url).permit(:link)
+  	@url = Url.new(safe_url)
+  	@url.hash_code = rand(100000)
+  	
+  	if @url.save
+  		redirect_to urls_path
+  	end
+	end
 
   def show
   end
